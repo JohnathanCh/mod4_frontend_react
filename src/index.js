d73b2053f7a4d35fba36dfeb3cbc82d4c2478396
github username <github email address>
2018-07-07 15:56:05 -0400

    Made some Component Skeletons

--- src/index.js
+++ src/index.js
@@ -3,6 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import NavBar from './navbar';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <div>
+        <NavBar />
+        <App />
+    </div>,
+    document.getElementById('root'));
 registerServiceWorker();
